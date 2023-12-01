6abd29643fd3e1c6fe72091d02ba5f129f1fe0df
Vijay Nannapuraju <59799317+vijayn7@users.noreply.github.com>
2023-11-30 21:18:04 -0500

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,14 +8,13 @@ const Footer = () => {
     <div className=" bg-black text-white rounded-t-3xl mt-8 md:mt-0">
       <div className="flex flex-col md:flex-row justify-between p-8 md:px-32 px-5">
         <div className=" w-full md:w-1/4">
-          <h1 className=" font-semibold text-xl pb-4">FoodieWeb</h1>
+          <h1 className=" font-bold text-xl pb-4 text-brightColor">Game Day Grill</h1>
           <p className=" text-sm">
-            Indulge in a symphony of flavors, where each plate is a canvas for
-            culinary excellence.
+            SLOGAN
           </p>
         </div>
         <div>
-          <h1 className=" font-medium text-xl pb-4 pt-5 md:pt-0">Links</h1>
+          <h1 className=" font-medium text-xl pb-4 pt-5 md:pt-0 text-brightColor">Links</h1>
           <nav className=" flex flex-col gap-2">
             <a
               className=" hover:text-brightColor transition-all cursor-pointer"
@@ -44,7 +43,7 @@ const Footer = () => {
           </nav>
         </div>
         <div>
-          <h1 className=" font-medium text-xl pb-4 pt-5 md:pt-0">Menu</h1>
+          <h1 className=" font-medium text-xl pb-4 pt-5 md:pt-0 text-brightColor">Menu</h1>
           <nav className=" flex flex-col gap-2">
             <a
               className=" hover:text-brightColor transition-all cursor-pointer"
@@ -61,19 +60,19 @@ const Footer = () => {
           </nav>
         </div>
         <div>
-          <h1 className=" font-medium text-xl pb-4 pt-5 md:pt-0">Contact Us</h1>
+          <h1 className=" font-medium text-xl pb-4 pt-5 md:pt-0 text-brightColor">Contact Us</h1>
           <nav className=" flex flex-col gap-2">
             <a
               className=" hover:text-brightColor transition-all cursor-pointer"
               href="/"
             >
-              FoodieWeb@email.com
+              GameDayGrill@email.com
             </a>
             <a
               className=" hover:text-brightColor transition-all cursor-pointer"
               href="/"
             >
-              +64 958 248 966
+              +1 248 123 5678
             </a>
             <a
               className=" hover:text-brightColor transition-all cursor-pointer"
@@ -83,15 +82,6 @@ const Footer = () => {
             </a>
           </nav>
         </div>
-      </div>
-      <div>
-        <p>
-          <p className=" text-center py-4">
-            @copyright developed by
-            <span className=" text-brightColor"> champion programmers</span> |
-            All rights reserved
-          </p>
-        </p>
       </div>
     </div>
   );
