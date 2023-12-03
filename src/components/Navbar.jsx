bfaf4491f44d9ed74452112aefd801ba3815a447
ShreyasNarayanan06 <94770905+ShreyasNarayanan06@users.noreply.github.com>
2023-12-03 00:16:57 -0500

    navbar change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,11 @@ const Navbar = () => {
         <nav >
             <header class="bg-backgroundColor fixed top-0 w-full h-24 z-50 border-b-2 border-black">
                 <div class="container flex justify-center items-center mx-auto p-3 space-x-8">
-                    <a href="#"><img src="src/assets/brand.png" class="h-16" /></a>
                     <a href="#" class="ml-0 hover:text-primary">Menu</a>
                     <a href="#" className="hover:text-primary">Order</a>
+                    <a href="#"><img src="src/assets/brand.jpg" class="h-16" /></a>
                     <a href="#" className="hover:text-primary">Locations</a>
-                    <a href="#" className="hover:text-primary">Reservations</a>
                     <a href="#" className="hover:text-primary">Catering</a>
-                    <a href="#" className="hover:text-primary">Contact</a>
                 </div>
             </header>
         </nav>
