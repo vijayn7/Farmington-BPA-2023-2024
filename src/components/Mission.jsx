7f140489db12bcaae39ac6eb8fa0026cb7b5097b
Vijay Nannapuraju <vijaynannapuraju@gmail.com>
2023-12-02 19:37:44 -0500

    line fix

--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Mission = () => {
 
     return (
-        <div className="border-y-2 border-black py-8 p-4 md:pl-16">
+        <div className="border-t-2 border-black py-8 p-4 md:pl-16">
             <h1 className="font-bold text-6xl md:text-7xl">Our Mission</h1>
 
             <div className="bg-fillColor md:w-[70%] text-xl md:text-2xl lg:text-4xl p-6 rounded-[2rem] my-8 md:my-10 outline-dashed outline-primary outline-offset-8">
