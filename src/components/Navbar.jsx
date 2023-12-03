731f952c68578f012d02feec37b94f54a1908894
ShreyasNarayanan06 <94770905+ShreyasNarayanan06@users.noreply.github.com>
2023-12-02 19:22:03 -0500

    nb border

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 const Navbar = () => {
     return (
         <nav >
-            <header class="bg-white fixed top-0 w-full h-20 z-50">
+            <header class="bg-backgroundColor fixed top-0 w-full h-20 z-50 border-b-2 border-black">
                 <div class="container flex justify-center items-center mx-auto p-3 space-x-8">
                     <a href="#"><img src="src/assets/Logo-Brand.png" class="h-16" /></a>
                     <a href="#" class="ml-0">Menu</a>
