cd5d0f8971be18c6c0cb6697eb78a40850b6cf12
Vijay Nannapuraju <vijaynannapuraju@gmail.com>
2023-12-02 19:27:52 -0500

    nav highlights

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,12 @@ const Navbar = () => {
             <header class="bg-backgroundColor fixed top-0 w-full h-20 z-50 border-b-2 border-black">
                 <div class="container flex justify-center items-center mx-auto p-3 space-x-8">
                     <a href="#"><img src="src/assets/Logo-Brand.png" class="h-16" /></a>
-                    <a href="#" class="ml-0">Menu</a>
-                    <a href="#">Order</a>
-                    <a href="#">Locations</a>
-                    <a href="#">Reservations</a>
-                    <a href="#">Catering</a>
-                    <a href="#">Contact</a>
+                    <a href="#" class="ml-0 hover:text-primary">Menu</a>
+                    <a href="#" className="hover:text-primary">Order</a>
+                    <a href="#" className="hover:text-primary">Locations</a>
+                    <a href="#" className="hover:text-primary">Reservations</a>
+                    <a href="#" className="hover:text-primary">Catering</a>
+                    <a href="#" className="hover:text-primary">Contact</a>
                 </div>
             </header>
         </nav>
