dbb41e30968d3ecd824cf0dc918f48af9ceb3cba
Vijay Nannapuraju <59799317+vijayn7@users.noreply.github.com>
2023-12-05 09:41:52 -0500

    home fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,12 +3,12 @@ import Button from "../layouts/Button";
 
 const Home = () => {
   return (
-    <div className="border-b-2 border-black min-h-[90vh] flex flex-row justify-between items-center lg:px-32 px-5 bg-[url('./assets/img/home.png')] bg-cover bg-no-repeat">
+    <div className="border-b-2 border-black h-[80vh] flex flex-row justify-between items-center lg:px-32 px-5 bg-[url('./assets/img/home.png')] bg-cover bg-no-repeat">
       <div className="w-full lg:w-2/3 space-y-5">
-        <h1 className="text-lightText font-bold text-7xl absolute top-[46rem] left-5">
+        <h1 className="text-lightText font-bold text-7xl absolute top-[75vh] left-5">
           Game On, Grill On
         </h1>
-        <div className="lg:pl-44 absolute top-[46rem] right-5">
+        <div className="lg:pl-44 absolute top-[75vh] right-5">
           <a href="#menu">
             <Button title="View Menu" />
           </a>
