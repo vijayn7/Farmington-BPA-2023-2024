9b4e4b1a2909b953ac30494a925b9bc4345b5930
Vijay Nannapuraju <vijaynannapuraju@gmail.com>
2023-12-04 21:52:41 -0500

    desktop view done

--- src/pages/Locations.jsx
+++ src/pages/Locations.jsx
@@ -5,14 +5,16 @@ import Stores from "../layouts/Stores";
 
 const Locations = () => {
   return (
-    <div className="px-32">
-      <h1 className="text-7xl font-bold py-12">Locations</h1>
-      <div className="grid grid-cols-4 gap-1">
-        <iframe className="outline w-[50rem] h-[50rem] col-span-2" src="https://www.google.com/maps/d/embed?mid=1cu_pS-wXIkpWfxh64doiM8LjEBQ3dIQ&ehbc=2E312F"></iframe>
-        <span className="col-span-2 pl-64"><Stores /></span>
+    <>
+      <div className="px-32 py-12">
+        <h1 className="text-7xl font-bold pb-6">Locations</h1>
+        <div className="grid grid-cols-4 gap-1">
+          <iframe className="outline w-[50rem] h-[50rem] col-span-2" src="https://www.google.com/maps/d/embed?mid=1cu_pS-wXIkpWfxh64doiM8LjEBQ3dIQ&ehbc=2E312F"></iframe>
+          <span className="col-span-2 pl-64"><Stores /></span>
+        </div>
       </div>
       <Footer />
-    </div>
+    </>
   );
 };
 
