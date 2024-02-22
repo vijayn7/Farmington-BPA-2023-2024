7f9000822f78f23b9ac9e85e903f46dd84e6aee4
Vijay Nannapuraju <vijaynannapuraju@gmail.com>
2024-02-22 15:19:57 -0500

    logic in place

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -1,21 +1,37 @@
-import React from "react";
+import React, { useState } from 'react';
 import Footer from "../components/Footer";
-import Menu from "../components/Menu"
 import ItemView from "../components/ItemView";
+import ButtonGroup from "../layouts/ButtonGroup";
+import Tabs from "../layouts/Tabs";
+import Cards from "../layouts/Cards";
 
 const Order = () => {
 
     const selectedItems = [];
 
     const handleCardClick = (cardId) => {
-        selectedItems.push(cardId)
+        if (!selectedItems.includes(cardId))
+            selectedItems.push(cardId);
         console.log(selectedItems);
+    };
+
+    const [activeTab, setActiveTab] = useState(0);
+
+    const handleTabChange = (tabNumber) => {
+        setActiveTab(tabNumber);
     };
 
     return (
         <div>
             <ItemView />
-            <Menu onClickCard={handleCardClick} />
+            <div className="min-h-screen flex flex-col justify-left items-left lg:px-32 px-5 pt-20" id="menu">
+                <h1 className="text-[6rem] text-center md:text-left">Menu</h1>
+                <ButtonGroup />
+                <div className="mt-8">
+                    <Tabs activeTab={activeTab} onTabChange={handleTabChange} />
+                </div>
+                <Cards activeTab={activeTab} onClickCard={handleCardClick} />
+            </div>
             <Footer />
         </div>
     );
