75706d4cd95665058f4ac39a1c04e45b9c6b226e
ShreyasNarayanan06 <94770905+ShreyasNarayanan06@users.noreply.github.com>
2024-02-24 23:05:11 -0500

    homepage and font change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,14 +4,10 @@ import Button from "../layouts/Button";
 const Home = () => {
   return (
     <div className="w-full border-b-2 border-black h-[40vh] md:h-[80vh] flex flex-row justify-between items-center lg:px-32 bg-[url('./assets/img/home.png')] bg-cover bg-no-repeat">
-      <div className="w-full lg:w-2/3 space-y-5">
-        <h1 className="text-lightText font-bold text-4xl md:text-[8rem] lg:text-7xl text-center lg:absolute lg:top-[75vh] lg:left-5">
-          Game On, Grill On
-        </h1>
-        <div className="lg:pl-44 text-center lg:absolute lg:top-[75vh] lg:right-5">
-          <a href="#menu">
-            <Button title="View Menu" />
-          </a>
+      <div className="w-full lg:w-2/3 space-y-5 pl-24">
+        <div className="bg-black w-[34rem] h-[265px] bg-opacity-[80%]">
+              <h1 className="font-bold text-white text-7xl pt-5 pl-4">Game On, Grill On!</h1>
+              <p className="text-gray-300 text-2xl pl-4 pt-3">Explore our wide selection of dishes to find your perfect meal for the big game!</p>
         </div>
       </div>
     </div>
