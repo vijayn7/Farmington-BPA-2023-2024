be33a8f35603ed71b2c5c99f2b6b32249aa9ab29
ShreyasNarayanan06 <94770905+ShreyasNarayanan06@users.noreply.github.com>
2024-03-05 23:23:31 -0500

    scroll cart

--- src/layouts/OrderItemsView.jsx
+++ src/layouts/OrderItemsView.jsx
@@ -7,7 +7,7 @@ import { FaXmark } from "react-icons/fa6";
 const OrderItemsView = (props) => {
     return (
         <>
-            <div className=" flex flex-col  justify-between w-[24vw] top-0 right-0 bottom-0 bg-white h-[100vh] fixed z-50 rounded-tl-[2rem] border-black border-l-[1px] border-t-[1px]">
+            <div className=" flex flex-col overflow-scroll	 justify-between w-[24vw] top-0 right-0 bottom-0 bg-white h-[100vh] fixed z-50 rounded-tl-[2rem] border-black border-l-[1px] border-t-[1px]">
                 <div>
                     <FaXmark className="text-2xl ml-auto mr-12 mt-12" />
                     <h1 className="text-l font-bold text-left ml-8 mt-8">Review Your Cart</h1>
@@ -24,7 +24,7 @@ const OrderItemsView = (props) => {
                         <h1 className="text-sm justify-self-end font-regular text-gray-600 mr-5 mt-6 ">$14.99</h1>
                     </div>
                 </div>
-<button className=" mx-20 mb-12 h-16 align-bottom bottom-0 bg-transparent hover:bg-[#EAA800] text-[#EAA800] font-semibold hover:text-white py-2 px-4 border border-[#EAA800] hover:border-transparent rounded-full ">Checkout</button>
+<button className=" mx-20 mb-12 mt-12 h-16 align-bottom bottom-0 bg-transparent hover:bg-[#EAA800] text-[#EAA800] font-semibold hover:text-white py-2 px-4 border border-[#EAA800] hover:border-transparent rounded-full ">Checkout</button>
             </div>
 
         </>
