f567f901c49e25ba30bbb28a6342afab70d7a15d
Vijay Nannapuraju <vijaynannapuraju@gmail.com>
2024-03-05 23:20:58 -0500

    form skeleton

--- src/pages/Catering.jsx
+++ src/pages/Catering.jsx
@@ -1,91 +1,153 @@
-import React from "react";
-import Footer from "../components/Footer"
+export default function Example() {
+  return (
+    <form className="mx-32">
+      <div className="space-y-12">
 
-export const Catering = () => {
-    return (
-        <div>
-            <div className="border-t-2 border-b-2 border-black md:pl-28 pt-[10rem] h-[40rem]" >
-                <h1 className="font-bold text-6xl md:text-7xl">Catering</h1>
+        <div className="border-b border-gray-900/10 pb-12">
+          <h2 className="text-base font-semibold leading-7 text-gray-900">Personal Information</h2>
+          <p className="mt-1 text-sm leading-6 text-gray-600">Use a permanent address where you can receive mail.</p>
 
-                <div className="bg-fillColor md:w-[70%] text-xl md:text-2xl lg:text-4xl p-6 rounded-[2rem] my-8 md:my-10 outline outline-primary outline-offset-8">
-                    Game Day Grill offers a variety of world class catering options for parties big or small. With seamless service and a passion for perfection, Game Day Grill delivers unforgettable experiences through food.            </div>
-
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-8">
+            <div className="sm:col-span-4">
+              <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
+                First name
+              </label>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="first-name"
+                  id="first-name"
+                  autoComplete="given-name"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
+                />
+              </div>
             </div>
-            <div className="md:pl-[7rem] pt-[6rem] pb-32">
-            <h1 className="font-bold text-6xl md:text-5xl">Fill out the Form Below for a Free Estimate!</h1>
 
-                <form className="bg-fillColor md:w-[87%]  p-6 rounded-[2rem] my-8 md:my-10 outline outline-primary outline-offset-8 grid grid-cols-2 place-items-center">
-                   
-                        <div className="pb-[1.5rem]">
-                            <label for="first-name" className="block">First Name</label>
-                            <input id="first-name" type="text" className="block rounded-md ring-1 ring-black w-[20rem] h-[2.5rem]">
-                            </input>
-                        </div>
-
-                        <div className="pb-[1.5rem]">
-                            <label for="last-name" className="block">Last Name</label>
-                            <input id="last-name" type="text" className="block rounded-md ring-1 ring-black w-[20rem] h-[2.5rem]">
-                            </input>
-                        </div >
-
-                        <div className="pb-[1.5rem]">
-                            <label for="phone-number" className="block">Phone Number</label>
-                            <input id="phone-number" type="tel" className="block rounded-md ring-1 ring-black w-[20rem] h-[2.5rem]">
-
-                            </input>
-                        </div>
-
-                        <div className="pb-[1.5rem]">
-                            <label for="date" className="block">Date</label>
-                            <input id="date" type="date" className="block rounded-md ring-1 ring-black w-[20rem] h-[2.5rem]">
-
-                            </input>
-                        </div>
-                        <div className="pb-[1.5rem]">
-                            <label for="email" className="block">Email</label>
-                            <input id="email" name="email" type="email" autocomplete="email" className="block rounded-md ring-1 ring-black w-[20rem] h-[2.5rem]">
-
-                            </input>
-                        </div>
-                        <div className="pb-[1.5rem]">
-                            <label for="street-address" className="block">Street Address</label>
-                            <input id="street-Address" name="street-Address" type="street-address" autocomplete="street-address" className="block rounded-md ring-1 ring-black w-[20rem] h-[2.5rem]">
-
-                            </input>
-                        </div>
-                        <div className="pb-[1.5rem]">
-                            <label for="city" className="block">City</label>
-                            <input id="city" name="city" type="address-level2" autocomplete="address-level2" className="block rounded-md ring-1 ring-black w-[20rem] h-[2.5rem]">
-
-                            </input>
-                        </div>
-                        <div className="pb-[1.5rem]">
-                            <label for="state" className="block">State</label>
-                            <input id="state" name="state" type="region" autocomplete="address-level1" className="block rounded-md ring-1 ring-black w-[20rem] h-[2.5rem]">
-
-                            </input>
-                        </div>
-                        <div className="pb-[3rem]">
-                            <label for="postal-code" className="block">Postal Code</label>
-                            <input id="postal-code" name="postal-code" type="postal-Code" autocomplete="postal-Code" className="block rounded-md ring-1 ring-black w-[20rem] h-[2.5rem]">
-
-                            </input>
-                        </div>
-                        <div className="pb-[1.5rem]">
-                            <label for="noG" className="block">Number of Guests</label>
-                            <input id="noG" name="noG" type="number" className="block rounded-md ring-1 ring-black w-[20rem] h-[2.5rem]">
-
-                            </input>
-                        </div>
-                        
-                        <div className="pb-[1.5rem]pt-[10rem]">
-                        <button type="button" className="rounded-md bg-secondary px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-black hover:bg-primary w-[20rem] h-[2.5rem]">Submit</button>
-                        </div>
-                </form>
+            <div className="sm:col-span-4">
+              <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-900">
+                Last name
+              </label>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="last-name"
+                  id="last-name"
+                  autoComplete="family-name"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
+                />
+              </div>
             </div>
-        <Footer />
+
+            <div className="sm:col-span-4">
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-4">
+              <label htmlFor="country" className="block text-sm font-medium leading-6 text-gray-900">
+                Country
+              </label>
+              <div className="mt-2">
+                <select
+                  id="country"
+                  name="country"
+                  autoComplete="country-name"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-primary sm:max-w-xs sm:text-sm sm:leading-6"
+                >
+                  <option>United States</option>
+                  <option>Canada</option>
+                  <option>Mexico</option>
+                </select>
+              </div>
+            </div>
+
+            <div className="col-span-full">
+              <label htmlFor="street-address" className="block text-sm font-medium leading-6 text-gray-900">
+                Event Address
+              </label>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="street-address"
+                  id="street-address"
+                  autoComplete="street-address"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-2 sm:col-start-1">
+              <label htmlFor="city" className="block text-sm font-medium leading-6 text-gray-900">
+                City
+              </label>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="city"
+                  id="city"
+                  autoComplete="address-level2"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-2">
+              <label htmlFor="region" className="block text-sm font-medium leading-6 text-gray-900">
+                State / Province
+              </label>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="region"
+                  id="region"
+                  autoComplete="address-level1"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-2">
+              <label htmlFor="postal-code" className="block text-sm font-medium leading-6 text-gray-900">
+                ZIP / Postal code
+              </label>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="postal-code"
+                  id="postal-code"
+                  autoComplete="postal-code"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+          </div>
         </div>
-    );
-}
 
-export default Catering;
+        
+      </div>
+
+      <div className="mt-6 flex items-center justify-end gap-x-6">
+        <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
+          Cancel
+        </button>
+        <button
+          type="submit"
+          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Save
+        </button>
+      </div>
+    </form>
+  )
+}
