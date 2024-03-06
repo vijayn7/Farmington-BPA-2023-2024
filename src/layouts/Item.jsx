a082d867e76df475be8fe294e7654f545be1c278
ShreyasNarayanan06 <94770905+ShreyasNarayanan06@users.noreply.github.com>
2024-03-05 23:16:23 -0500

    Added cart and minor bug fixes

--- src/layouts/Item.jsx
+++ src/layouts/Item.jsx
@@ -3,7 +3,16 @@ import React from "react";
 const Item = (props) => {
   return (
     <>
-      <h1>{props.name}</h1>
+      <div>
+        <div className="flex justify-between mt-10 ">
+          <div className="flex">
+            <img src={props.img} className="rounded-full overflow-hidden w-12 h-12 object-cover ml-8 " />
+            <h1 className="text-m font-semibold ml-5  mt-2">{props.name}</h1>
+          </div>
+          <h1 className="text-sm justify-self-end font-regular text-gray-600 mr-5 mt-2 ">${props.price}</h1>
+        </div>
+      </div>
+      
     </>
   );
 };
