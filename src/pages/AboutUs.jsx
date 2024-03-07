3b420e07066eac85254c89d87e5ffe7a4d4c3281
ShreyasNarayanan06 <94770905+ShreyasNarayanan06@users.noreply.github.com>
2024-03-06 23:35:24 -0500

    abous us poage

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,9 +1,16 @@
 import React, { Component } from 'react'
-
+import Founders from "../components/Founders";
+import Mission from "../components/Mission";
+import Footer from "../components/Footer";
 export default class AboutUs extends Component {
   render() {
     return (
-      <div>AboutUs</div>
+      <div>
+      <Mission/>
+      <Founders/>
+      <Footer />
+      </div>
+
     )
   }
 }
