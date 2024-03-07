57c742131e7dc388bfa683c2013cb19143cb3b7c
ShreyasNarayanan06 <94770905+ShreyasNarayanan06@users.noreply.github.com>
2024-03-06 22:02:51 -0500

    submit

--- src/pages/Catering.jsx
+++ src/pages/Catering.jsx
@@ -248,7 +248,7 @@ export default function Example() {
             type="submit"
             className="rounded-md bg-secondary mr-52 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-primary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-            Save
+            Submit
           </button>
         </div>
       </form>
